be744bf0bff7860d9fddd163bb2dae52599a293c
jacoborbach <jacoborbach96@gmail.com>
2020-12-14 17:36:28 -0500

    done one through 3

--- classes.js
+++ classes.js
@@ -29,7 +29,21 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name,
+      this.last_name = last_name,
+      this.email = email,
+      this.age = age
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
+const Jacob = new Employee('Jacob', 'Orbach', 'jacoborbach96@gmail.com', 24)
+//console.log(Jacob);
+//Jacob.makeWidget();
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +61,25 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}
+
+const Henry = new Manager('Henry', 'Beathoven', 'henryb@gmail.com', 33)
+//console.log(Henry)
+// Henry.hire('Jonathan')
+// Henry.hire('Noam')
+// Henry.fire('Jonathan')
+// console.log(Henry)
 
 
 ////////// PROBLEM 3 //////////
@@ -71,8 +103,42 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age);
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hireOrFire() {
+    if (this.reports.length === 0) {
+      this.title = 'Not a manager';
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager';
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager';
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager';
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus';
+    } else if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager';
+    }
+  }
+  hire(employee) {
+    super.hire(employee);
+    this.hireOrFire();
+  }
+  fire(index) {
+    super.fire(index);
+    this.bonus += 100;
+    this.hireOrFire();
+  }
+}
+const lion = new ProgressiveManager('Lion', 'Lastname', 'lionL22@gmail.com', 40)
+//console.log(lion)
+//lion.hire('Jonathan');
+//lion.fire('Jonathan')
+//console.log(lion)
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
